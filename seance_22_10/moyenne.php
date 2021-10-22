0e65376d8355177a32fb15f293b0a86600e42e2c
Tom <tommusique974@gmail.com>
2021-10-22 17:13:15 +0200

    base

--- seance_22_10/moyenne.php
+++ seance_22_10/moyenne.php
@@ -7,9 +7,7 @@
 <center>
     <h1>Exercice moyenne</h1>
     <p>écrire un script PHP qui calcule la moyenne de note</p>
-    <form action="moyenne.php" method="POST">
 
-    </form>
 </center>
 </body>
 </html>
