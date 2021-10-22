63e53ef45b42681734cc546e2b178b04098089d0
Tom <tommusique974@gmail.com>
2021-10-22 21:18:09 +0200

    prepare

---
+++ seance_22_10/CalculerTemps.php
@@ -0,0 +1,13 @@
+<html lang="fr">
+<head>
+    <title>Moyenne</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<center>
+    <h1>Exercice calculer surface</h1>
+    <p>écrire un script PHP qui calcule ule temps</p>
+
+</center>
+</body>
+</html>
