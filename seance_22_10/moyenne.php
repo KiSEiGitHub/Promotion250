c50907a55347b519c00eafecd210a3d6249abb1e
Tom <tommusique974@gmail.com>
2021-10-22 15:45:26 +0200

    pas bon a fix

--- seance_22_10/moyenne.php
+++ seance_22_10/moyenne.php
@@ -8,23 +8,7 @@
     <h1>Exercice moyenne</h1>
     <p>écrire un script PHP qui calcule la moyenne de note</p>
     <form action="moyenne.php" method="POST">
-        <?php
-        echo "<input type='number' name='nb' placeholder='Nombre de note'>";
-        echo "<input type='submit' name='btn' value='OK'>";
-        if (isset($_POST['btn'])) {
-            $nb_note = $_POST['nb'];
-            echo "<p>Veuillez rentrer $nb_note</p>";
-            $somme = 0;
-            for ($i = 1; $i < $nb_note + 1; $i++) {
-                echo "<form method='POST' action='moyenne.php'>";
-                    echo "<input type='number' name='note' placeholder='note $i'>";
-                    echo "<input type='submit' name='btn2' value='OK'>";
-                echo "</form>";
-            }
-        } else {
-            echo " ";
-        }
-        ?>
+
     </form>
 </center>
 </body>
