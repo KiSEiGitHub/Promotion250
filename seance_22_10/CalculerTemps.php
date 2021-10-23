4c1592e0e9a2d13e01a6d6af61fc73fce1a638d9
Tom <tommusique974@gmail.com>
2021-10-23 20:53:38 +0200

    Calculer heure minute seconde total en seconde

--- seance_22_10/CalculerTemps.php
+++ seance_22_10/CalculerTemps.php
@@ -5,9 +5,31 @@
 </head>
 <body>
 <center>
-    <h1>Exercice calculer surface</h1>
-    <p>écrire un script PHP qui calcule ule temps</p>
+    <h1>Exercice calculer total seconde</h1>
+    <p>écrire un script PHP qui calcule le temps heure minute seconde en senconde</p>
+    <form action="CalculerTemps.php" method="post">
+        <input type="number" name="heure" placeholder="Heure">
+        <input type="number" name="minute" placeholder="Minute">
+        <input type="number" name="seconde" placeholder="Seconde">
+        <input type="submit" name="btn" value="OK">
+        <?php
+        if (isset($_POST['btn'])) {
+            if (!empty($_POST['heure']) || !empty($_POST['minute']) || !empty($_POST['seconde'])) {
+                $heure = $_POST['heure'];
+                $min = $_POST['minute'];
+                $sec = $_POST['seconde'];
 
+                $heure *= 3600;
+                $min *= 60;
+                $totla = $heure + $min + $sec;
+
+                echo "<p>Seconde : $totla</p>";
+            } else{
+                echo "<p>Il manque un truc</p>";
+            }
+        }
+        ?>
+    </form>
 </center>
 </body>
 </html>
