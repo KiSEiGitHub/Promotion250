4817cae5a8942f7b9cb0aed0c1d36dec15af8bf2
Tom <tommusique974@gmail.com>
2021-12-17 11:10:09 +0100

    remove dom

--- mesites/home.php
+++ mesites/home.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <h3 style="font-size: 32px">Accueil du site</h3>
 
 <img src="images/logo.png" alt="" width="400px" height="250px">
@@ -17,5 +7,3 @@
     rendre service
 </p>
 
-</body>
-</html>
