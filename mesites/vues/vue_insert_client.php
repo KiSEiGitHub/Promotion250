8ecdce9ccbfd59392ff1973676d8952e9e5f7fd1
Tom <tommusique974@gmail.com>
2021-12-17 11:39:49 +0100

    prepare

---
+++ mesites/vues/vue_insert_client.php
@@ -0,0 +1,11 @@
+<h4>Insertion client</h4>
+<form action="" method="post">
+    <input type="text" name="prenom" placeholder="Prénom"><br>
+    <input type="text" name="nom" placeholder="nom"><br>
+    <input type="text" name="adresse" placeholder="adresse"><br>
+    <input type="email" name="email" placeholder="email"><br>
+    <input type="password" name="mdp" placeholder="mot de passe"><br>
+    <input type="text" name="tel" placeholder="téléphone"><br>
+    <input type="submit" value="Envoyer" name="btn">
+    <input type="reset" value="reset" name="annuler">
+</form>
