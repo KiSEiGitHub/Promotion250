3f54ba0871ffce6dffc941abc7d0287cf8eda6a5
Tom <tommusique974@gmail.com>
2021-12-19 00:33:45 +0100

    adjust title

--- mesites/vues/vue_les_clients.php
+++ mesites/vues/vue_les_clients.php
@@ -14,10 +14,10 @@
         echo "<tr>";
             echo "<td>".$unClient['idclient']."</td>";
             echo "<td>".$unClient['nom']."</td>";
+            echo "<td>".$unClient['prenom']."</td>";
             echo "<td>".$unClient['adresse']."</td>";
             echo "<td>".$unClient['email']."</td>";
             echo "<td>".$unClient['tel']."</td>";
-            echo "<td>".$unClient['prenom']."</td>";
         echo "</tr>";
     }
     ?>
