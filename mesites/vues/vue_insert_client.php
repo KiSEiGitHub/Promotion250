1e4e86487a2a4a06f2c888572f81f15256d6bd66
Tom <tommusique974@gmail.com>
2021-12-18 18:05:48 +0100

    super comment del label add class for css

--- mesites/vues/vue_insert_client.php
+++ mesites/vues/vue_insert_client.php
@@ -1,11 +1,18 @@
 <h4>Insertion client</h4>
 <form action="" method="post">
-    <input type="text" name="prenom" placeholder="Prénom"><br>
-    <input type="text" name="nom" placeholder="nom"><br>
-    <input type="text" name="adresse" placeholder="adresse"><br>
-    <input type="email" name="email" placeholder="email"><br>
-    <input type="password" name="mdp" placeholder="mot de passe"><br>
-    <input type="text" name="tel" placeholder="téléphone"><br>
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="text" name="prenom" placeholder="Prénom" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="text" name="nom" placeholder="nom" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="text" name="adresse" placeholder="adresse" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="email" name="email" placeholder="email" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="password" name="mdp" placeholder="mot de passe" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
+    <input type="text" name="tel" placeholder="téléphone" class="field">
+    <!--suppress HtmlFormInputWithoutLabel -->
     <input type="submit" value="Envoyer" name="btn">
     <input type="reset" value="reset" name="annuler">
 </form>
