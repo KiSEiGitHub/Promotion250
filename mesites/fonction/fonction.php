bd110c7b1feed81a7f4e8d7558e89ff4daed1b5a
Tom <tommusique974@gmail.com>
2021-12-19 06:35:24 +0100

    insert user ✔️

--- mesites/fonction/fonction.php
+++ mesites/fonction/fonction.php
@@ -35,8 +35,42 @@ function selectAllclient()
 }
 
 
-function insertClient($tab)
+function insertClient()
 {
+
+    $con = mysqli_connect(
+        "localhost",
+        "root",
+        "",
+        "garage_250"
+    );
+
+    if (isset($_POST['btn'])) {
+        if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['adresse']) && !empty($_POST['mail']) &&
+            !empty($_POST['mdp']) && !empty($_POST['tel'])) {
+
+            // variable
+            $prenom = $_POST['prenom'];
+            $nom = $_POST['nom'];
+            $adresse = $_POST['adresse'];
+            $mail = $_POST['mail'];
+            $mdp = $_POST['mdp'];
+            $tel = $_POST['tel'];
+
+            // requête sql pour insert
+            $sql = "INSERT INTO client(nom, prenom, adresse, email, mdp, tel) VALUES ('$nom', '$prenom', '$adresse', '$mail', '$mdp', '$tel')";
+
+            if (mysqli_query($con, $sql)) {
+                echo "<p class='sucess'>Nouveau utilisateur</p>";
+            } else {
+                echo "<p class='error'>Une erreur est survenue</p>";
+            }
+
+            mysqli_close($con);
+        } else {
+            echo "<p class='error'>Veuillez remplir tous les champs</p>";
+        }
+    }
 
 }
 
